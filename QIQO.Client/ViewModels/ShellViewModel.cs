aac76751665af21d3ef6d8f06c22e66f0a1a06c4
R Richards <rdrrichards@hotmail.com>
2017-03-18 21:00:31 -0500

    Tile testing

--- QIQO.Client/ViewModels/ShellViewModel.cs
+++ QIQO.Client/ViewModels/ShellViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using Prism.Events;
 using Prism.Mvvm;
+using QIQO.Client.Controls;
 using QIQO.Client.Services;
+using System.Collections.ObjectModel;
+using System;
 
 namespace QIQO.Client.ViewModels
 {
@@ -9,19 +12,33 @@ namespace QIQO.Client.ViewModels
         private readonly ILRPClient _lrpClient;
         private readonly IEventAggregator _eventAggregator;
         private string _number;
+        private ObservableCollection<BaseTile> _tiles = new ObservableCollection<BaseTile>();
 
         public ShellViewModel(ILRPClient lrpClient, IEventAggregator eventAggregator)
         {
             _lrpClient = lrpClient;
             _eventAggregator = eventAggregator;
 
-            _eventAggregator.GetEvent<NumberUpdatedEvent>().Subscribe((val) => Number = val);
+            _eventAggregator.GetEvent<NumberUpdatedEvent>().Subscribe(DoSomething, ThreadOption.UIThread); // (val) => Number = val);
         }
+
+        private void DoSomething(string val)
+        {
+            Number = val;
+            Tiles.Add(new BaseTile { TileLabel = $"Tile: {val}", TileValue = val });
+            OnPropertyChanged(nameof(Tiles));
+        }
+
         public string ViewTitle => "Main Window";
         public string Number
         {
             get { return _number; }
             set { SetProperty(ref _number, value); }
+        }
+        public ObservableCollection<BaseTile> Tiles
+        {
+            get { return _tiles; }
+            set { SetProperty(ref _tiles, value); }
         }
     }
 }
